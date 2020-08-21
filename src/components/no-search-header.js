8317035a73ac0390def3a6d0c378d8426d14224d
marcosdellavecchia <33458523+marcosdellavecchia@users.noreply.github.com>
2020-08-21 19:01:19 -0300

    Class state to functional state

--- src/components/no-search-header.js
+++ src/components/no-search-header.js
@@ -3,21 +3,19 @@ import "./search-bar.css";
 import logo from "../logo-gs.png";
 import { Link } from "react-router-dom";
 
-class NoSearchHeader extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div className="row">
-          <div className="col-md-2">
-            <Link to="/">
-              <img src={logo} alt="" className="logo-barra" />
-            </Link>
-          </div>
+function NoSearchHeader() {
+  return (
+    <React.Fragment>
+      <div className="row">
+        <div className="col-md-2">
+          <Link to="/">
+            <img src={logo} alt="" className="logo-barra" />
+          </Link>
         </div>
-        <hr />
-      </React.Fragment>
-    );
-  }
+      </div>
+      <hr />
+    </React.Fragment>
+  );
 }
 
 export default NoSearchHeader;
